632b57fd4d90744fb8c8c1f52e9563c748d7b46d
c9s <cornelius.howl@gmail.com>
2013-12-06 23:56:57 +0800

    set defaultOrdering attribute

--- Model/ImageCollection.php
+++ Model/ImageCollection.php
@@ -6,6 +6,10 @@ namespace BannerSlider\Model;
 class ImageCollection 
 extends \BannerSlider\Model\ImageCollectionBase
 {
+    public $defaultOrdering = array(
+        array('ordering','asc'),
+        array('id','asc'),
+    );
 
     /**
      * $images = ImageCollection::by_placeholder('index');
@@ -13,13 +17,12 @@ extends \BannerSlider\Model\ImageCollectionBase
     public static function by_placeholder($identity)
     {
         $c = new Category( array('place_holder' => $identity, 'lang' => kernel()->locale->current() ) );
-        if ( ! $c->id )
+        if ( ! $c->id ) {
             return;
+        }
         $collection = new self;
         $collection->where()
             ->equal('category_id',$c->id);
-        $collection->order('ordering','ASC');
-        $collection->order('id','ASC');
         return $collection;
     }
     
