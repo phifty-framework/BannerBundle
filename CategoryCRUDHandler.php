6e18f1c557b22705bb9b10fa849ed6119d9b69b0
c9s <cornelius.howl@gmail.com>
2013-12-15 22:14:47 +0800

    enable editing

--- CategoryCRUDHandler.php
+++ CategoryCRUDHandler.php
@@ -6,6 +6,6 @@ class CategoryCRUDHandler extends \AdminUI\CRUDHandler
     /* CRUD Attributes */
     public $modelClass = 'BannerSlider\\Model\\Category';
     public $crudId     = 'banner_category';
-    public $canEdit = false;
-    public $canDelete = false;
+    public $canEdit    = true;
+    public $canDelete  = true;
 }
