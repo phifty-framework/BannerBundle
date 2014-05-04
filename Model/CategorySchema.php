6619d38e0ef5f03c7fb857009cbe40b21621caab
c9s <yoanlin93@gmail.com>
2014-05-05 07:11:49 +0800

    Improve banner category form

--- Model/CategorySchema.php
+++ Model/CategorySchema.php
@@ -8,14 +8,19 @@ class CategorySchema extends SchemaDeclare
     {
         $this->table('banner_categories');
 
+        $this->column('name')
+            ->varchar(128)
+            ->label('類別名稱')
+            ->renderAs('TextInput', [ 'placeholder' => '首頁橫幅' ])
+            ;
+
         $this->column('handle')
             ->varchar(64)
             ->required(true)
-            ->label('程式標記');
+            ->label('程式標記')
+            ->renderAs('TextInput', [ 'placeholder' => 'main-page' ])
+            ;
 
-        $this->column('name')
-            ->varchar(128)
-            ->label('類別名稱');
 
         $this->column('description')
             ->varchar(1024)
@@ -35,6 +40,12 @@ class CategorySchema extends SchemaDeclare
             ->label('高度')
             ->renderAs('TextInput',array('size' => 4))
             ->hint('以 pixel (px) 為單位')
+            ;
+
+        $this->column('remark')
+            ->text()
+            ->label('備註')
+            ->renderAs('TextareaInput')
             ;
 
         $this->mixin('I18N\\Model\\Mixin\\I18NSchema');
