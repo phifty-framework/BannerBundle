ec91413092c580dde2263b00855c47f8031a5660
c9s <yoanlin93@gmail.com>
2017-06-04 09:54:04 +0800

    Add repo classes

---
+++ Model/CategoryRepo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace BannerBundle\Model;
+
+
+use BannerBundle\Model\CategoryRepoBase;
+
+class CategoryRepo
+    extends CategoryRepoBase
+{
+}
---
+++ Model/ImageRepo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace BannerBundle\Model;
+
+
+use BannerBundle\Model\ImageRepoBase;
+
+class ImageRepo
+    extends ImageRepoBase
+{
+}
